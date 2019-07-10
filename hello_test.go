392621f79580e098770ad703662fa4a156897d94
Victoria Yang <victoria.j.yang@gmail.com>
2019-07-10 16:11:15 -0700

    Update hello_test.go

--- hello_test.go
+++ hello_test.go
@@ -7,7 +7,7 @@ import (
 func TestHello(t *testing.T) {
 	ts := timeZone()
 	expected := "2019-07-10 10:44:42.564651745 +1000 AEST"
-	if ts != expected {
+	if ts.String() != expected {
 		t.Errorf("Got '%v', expected '%v'", ts, expected)
 	}
 }
