df3aae21f8f0545e4ccbafe7e411e436103acc94
Victoria Yang <victoria.j.yang@gmail.com>
2019-07-10 15:49:29 -0700

    Create hello_test.go

---
+++ hello_test.go
@@ -0,0 +1,14 @@
+package types
+
+import (
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	ts := timeZone()
+	expected := "2019-07-10 10:44:42.564651745 +1000 AEST"
+	if !expected.Compare(ts).Equal(IntZero).(Bool) {
+		t.Errorf("Got '%v', expected '%v'", ts, expected)
+	}
+}
+
